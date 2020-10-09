d86e5d4c39150aae13b8c5545acd933b9408ea54
Aleksey <fduchunru@gmail.com>
2020-10-09 14:24:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,15 +33,20 @@ function viewCart() {
   //  }
     // return `In your cart, you have ${cart[0]["itemName"]} at $${cart[0]["itemPrice"]}, ${k} and ${cart[cart.length-1]["itemName"]} at $${cart[cart.length-1]["itemPrice"]}.`
    var firstPart = `In your cart, you have ${cart[0]["itemName"]} at $${cart[0]["itemPrice"]}`
-   //var middlePart = `${cart[i]["itemName"]} at $${cart[i]["itemPrice"]},`
+   var middlePart = `${cart[i]["itemName"]} at $${cart[i]["itemPrice"]},`
    var lastPart = `and ${cart[cart.length-1]["itemName"]} at $${cart[cart.length-1]["itemPrice"]}.`
+   var moreItems = []
    if (l == 1){
      return `${firstPart}.`
    }
    else if (l == 2){
      return  `${firstPart}, ${lastPart}`
    }
-
+   else {for (let i = 1; i < cart.length - 1; i++){
+     moreItems.unshift(middlePart)
+   }
+   return `${firstPart}, ${moreItems} ${lastPart}`
+   }
 
 
 
